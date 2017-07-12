6513fe95a088d4255c7adb27a759da1aaf96502b
ramadhan yunus <ramadhan yunus>
2017-07-12 20:10:45 +0700

    initial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="/css/bootstrap.css" >
+<link rel="stylesheet" href="/css/bootstrap-theme.css" >
+<link rel="stylesheet" href="/css/style.css" >
+<script src="/js/jquery.js" ></script>
+<script src="/js/bootstrap.js" ></script>
+<script src="/js/js.js" ></script>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
